e4f804b164b21960393edb51898bf0dd8c2a7c7f
payal-kanani <payalgajera0812@gmail.com>
2025-05-16 10:55:20 +0200

    Remove detailed time components from current date and time output

--- practice.py
+++ practice.py
@@ -7,9 +7,4 @@ current_datetime = datetime.now()
 print("Current Date and Time:", current_datetime)
 print("Year:", current_datetime.year)
 print("Month:", current_datetime.month)
-print("Day:", current_datetime.day)
-print("Hour:", current_datetime.hour)
-print("Minute:", current_datetime.minute)
-print("Second:", current_datetime.second)
-print("Microsecond:", current_datetime.microsecond)
-print("Weekday:", current_datetime.weekday())  # Monday is 0 and Sunday is 6
+
